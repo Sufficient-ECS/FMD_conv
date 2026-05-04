df1727e9583c02409fe159f20b366625e8caae4f
Thomas Antoine <t.antoine@uclouvain.be>
2026-05-04 12:07:11 +0200

    Switch to pint units

--- fmd_conv.py
+++ fmd_conv.py
@@ -4,8 +4,11 @@ from lxml import etree
 import numpy as np
 from pathlib import Path
 import pandas as pd
+import pint
 import re
 import yaml
+
+u = pint.UnitRegistry()
 
 def load_mapping(map_folder):
     def df_to_map(path, key_col):
@@ -27,24 +30,29 @@ default_ac = {
     "HM": ('metal working, average for copper product manufacturing', 'RoW')
     }
 
-def treat_node(node, sp_name, hm_name, ns, apply_mapping, node_type):
-    this_name = node.get('name', 'Unknown')
-
-    act_name, location = apply_mapping(this_name)
-
+def get_mass(node, ns):
+    unit = node.get('UOM')
     value_str = node.get('value') # Try to get value directly from the Substance node
     if value_str is None: # If not present, look for a child <Amount> node
         amount_node = node.find('ipc:Amount', ns)
         if amount_node is not None:
             value_str = amount_node.get('value', '0')
+            unit = amount_node.get('UOM')
         else:
             value_str = '0'
-            print(f"Error: The value for the homogeneous {this_name} in file {xml_file.name} is zero")
-    value = float(value_str)
+    return u.parse_expression(f"{value_str} {unit}")
+
+def treat_node(node, sp_name, hm_name, mass, apply_mapping, node_type):
+    this_name = node.get('name', 'Unknown')
+
+    act_name, location = apply_mapping(this_name)
+
+    if mass == 0:
+        logging.warning(f"The mass for the homogeneous {this_name} in file {xml_file.name} is zero")
 
     if pd.isna(act_name):
         act_name, location = default_ac[node_type]
-        value = 0
+        mass = 0 * u.gram
 
     return {
         'act_name': act_name,
@@ -53,8 +61,8 @@ def treat_node(node, sp_name, hm_name, ns, apply_mapping, node_type):
         'c_homogeneous_material': hm_name,
         'c_substance': 'process' if type == "HM" else this_name,
         'amount': {
-            'value': value,
-            'unit': 'mg'
+            'value': mass.magnitude,
+            'unit': mass.units
         }
     }
 
@@ -77,6 +85,8 @@ def ipc1752_to_yaml(xml_file: str, output_folder: str, apply_mapping):
     - + homogeneous materials
     """
 
+    logging.debug(xml_file)
+
     # Load XML
     parser = etree.XMLParser(ns_clean=True)
     tree = etree.parse(xml_file, parser)
@@ -88,7 +98,7 @@ def ipc1752_to_yaml(xml_file: str, output_folder: str, apply_mapping):
     product_node = tree.find('.//ipc:Product', ns)
     product_id_node = product_node.find('ipc:ProductID', ns)
     product_name = f"{product_id_node.get('itemName')}_{product_id_node.get('version')}"
-    total_mass = float((product_id_node.find('ipc:Amount', ns).get('value', 0)) if product_id_node.find('ipc:Amount', ns) is not None else 0)
+    total_mass = get_mass(product_id_node, ns)
 
     logging.debug(product_name)
 
@@ -107,20 +117,25 @@ def ipc1752_to_yaml(xml_file: str, output_folder: str, apply_mapping):
         hmlist = sp.findall('.//ipc:HomogeneousMaterial', ns)
         for hm in hmlist:
             hm_name = hm.get('name', f"HM_{len(inputs)}")
-            inputs[f"process_input_{len(inputs):03d}"] = treat_node(hm, sp_name, hm_name, ns, apply_mapping, 'HM')
+            hm_mass = get_mass(hm, ns)
 
-            logging.debug(f"\t\t{hm_name} {hm.find('ipc:Amount', ns).get('value')}")
+            inputs[f"process_input_{len(inputs):03d}"] = treat_node(hm, sp_name, hm_name, hm_mass, apply_mapping, 'HM')
+
+            logging.debug(f"\t\t{hm_name} {hm_mass}")
 
             subs = hm.findall('.//ipc:Substance', ns) 
             for sub in subs:
-                new_sub = treat_node(sub, sp_name, hm_name, ns, apply_mapping, 'Substance')
-                inputs[f"process_input_{len(inputs):03d}"] = new_sub
-                logging.debug(f"\t\t\t{sub.get('name', 'Unknown')} {sub.find('ipc:Amount', ns).get('value')}")
-                computed_mass += new_sub["amount"]["value"]
+                sub_mass = get_mass(sub, ns)
 
-    tolerance = 0.01  # mg
+                new_sub = treat_node(sub, sp_name, hm_name, sub_mass, apply_mapping, 'Substance')
+                inputs[f"process_input_{len(inputs):03d}"] = new_sub
+
+                logging.debug(f"\t\t\t{sub.get('name', 'Unknown')} {sub_mass} ({(sub_mass/hm_mass).to('%'):.2f})")
+                computed_mass += sub_mass
+
+    tolerance = 0.01 * u.mg
     if abs(computed_mass - total_mass) > tolerance:
-        logging.warning(f"{xml_file}: mismatch total_mass={total_mass:.2f} vs sum_substances={computed_mass:.2f}")
+        logging.warning(f"{xml_file}: mismatch total_mass={total_mass.to('mg'):.2f} vs sum_substances={computed_mass.to('mg'):.2f}")
 
     yaml_data = {
         'output': {
